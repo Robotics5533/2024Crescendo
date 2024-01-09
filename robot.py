c3863c0c9ed38ff8a3dd474add3304475b3091b3
mequam <blue9ja@gmail.com>
2024-01-09 14:48:27 -0500

    example code talking to the smart dashboard

--- robot.py
+++ robot.py
@@ -5,8 +5,21 @@ import wpilib
 import wpilib.drive
 import os
 
+
 class Arpeggio(wpilib.TimedRobot):
     def robotInit(self):
+        #create an interface to the smart dashboard
+        self.auton_options = wpilib.SendableChooser()
+
+        #populate it with options
+        self.auton_options.addOption("test",2)
+        self.auton_options.addOption("test2",3)
+        self.auton_options.addOption("test3",4)
+        self.auton_options.setDefaultOption("test5",8) #note the default
+
+        #display the selected object to the screen
+        print(self.auton_options.getSelected())
+
         self.drive = MecanumDrive(1, 2, 3, 4)
         self.stick = wpilib.Joystick(0)
         self.timer = wpilib.Timer()
