a0ffe09484dfff3d3bc1348ed7f97fcebfbba18a
5533Robotics <5533ehprobotics@gmail.com>
2024-01-21 14:32:16 -0500

    testing limelight

--- robot.py
+++ robot.py
@@ -4,14 +4,18 @@ import wpilib
 import wpilib.drive
 from components.vision.limelight import Limelight
 from constants import Robot
+from phoenix6 import hardware
 
 
 class Arpeggio(wpilib.TimedRobot):
     def robotInit(self):
-        self.drive = MecanumDrive(self.Robot.motors.front_left, self.Robot.motors.front_right, self.Robot.motors.back_left, self.Robot.motors.back_right)
-        self.stick = self.Robot.controllers.driver.joystick()
+        self.drive = MecanumDrive(hardware.TalonFX(Robot.motors.front_left),
+                                  hardware.TalonFX(Robot.motors.front_right),
+                                  hardware.TalonFX(Robot.motors.back_left),
+                                  hardware.TalonFX(Robot.motors.back_right))
+        self.stick = wpilib.Joystick(0)
         self.timer = wpilib.Timer()
-        self.PathPlanner = PathPlanner("/home/lvuser/py/paths/Forwardybackwardy.json", self.timer)
+#        self.PathPlanner = PathPlanner("/home/lvuser/py/paths/Forwardybackwardy.json", self.timer)
         self.limelight=Limelight()
 
     def autonomousInit(self):
@@ -19,19 +23,21 @@ class Arpeggio(wpilib.TimedRobot):
         self.timer.start()
 
     def autonomousPeriodic(self):
-        self.PathPlanner.run(self.drive)
+#        self.PathPlanner.run(self.drive)
+        pass
 
     def teleopPeriodic(self):
         
-        if self.follow_limelight_btnid():
+        if self.stick.getRawButton(1):
             offset = self.limelight.getoffset()
-            self.drive.move(-offset[0], offset[1], -offset[2])
-        x, y, z = (
+            self.drive.move(-offset[0]*.5, -offset[1]*.5, -offset[2]*.5)
+        else:
+            x, y, z = (
             self.stick.getX(),
             self.stick.getY(),
             self.stick.getZ(),
-        )
-        self.drive.move(x, y, z)
+            )
+            self.drive.move(x, y, z)
 
 
 if __name__ == "__main__":
