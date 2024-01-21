1b95a48d64fecc8d70cc0458a67f8123113e33e5
stormybeat <128640313+stormybeat@users.noreply.github.com>
2024-01-21 13:06:12 -0500

    reverting to not use subsystems

--- robot.py
+++ robot.py
@@ -1,50 +1,38 @@
-# from components.auton.PathPlanner import PathPlanner
-# from components.drive.MecanumDrive import MecanumDrive
-# from constants import Robot
-# import wpilib
-# import wpilib.drive
-# import os
-
-# class Arpeggio(wpilib.TimedRobot):
-#     def robotInit(self):
-#         self.drive = MecanumDrive(1, 2, 3, 4)
-#         self.stick = wpilib.Joystick(0)
-#         self.timer = wpilib.Timer()
-#         self.PathPlanner = PathPlanner("/home/lvuser/py/paths/Forwardybackwardy.json", self.timer)
-
-#     def autonomousInit(self):
-#         self.timer.reset()
-#         self.timer.start()
-
-#     def autonomousPeriodic(self):
-#         self.PathPlanner.run(self.drive)
-
-#     def teleopPeriodic(self):
-#         x, y, z = (
-#             self.stick.getX(),
-#             self.stick.getY(),
-#             self.stick.getZ(),
-#         )
-#         self.drive.move(x, y, z)
-     
-
-# if __name__ == "__main__":
-#     wpilib.run(Arpeggio)
+from components.auton.PathPlanner import PathPlanner
+from components.drive.MecanumDrive import MecanumDrive
 import wpilib
-from robotcontainer import RobotContainer
-from commands2 import TimedCommandRobot
-class Arpeggio(TimedCommandRobot):
-    def robotInit(self):
-        self.container = RobotContainer()
-    #     self.timer = wpilib.Timer()
+import wpilib.drive
+from components.vision.limelight import Limelight
+from constants import Robot
 
-    # def autonomousInit(self) -> None:
-    #     self.timer.reset()
-    #     self.timer.start()
-    # def autonomousPeriodic(self) -> None:
-    #     if self.timer.get() <= 5:
-    #         self.container.drive_subsystem.drive.move(0.5, 0, 0)
-    def teleopPeriodic(self) -> None:
-        """return super().teleopPeriodic()"""
+
+class Arpeggio(wpilib.TimedRobot):
+    def robotInit(self):
+        self.drive = MecanumDrive(self.Robot.motors.front_left, self.Robot.motors.front_right, self.Robot.motors.back_left, self.Robot.motors.back_right)
+        self.stick = self.Robot.controllers.driver.joystick()
+        self.timer = wpilib.Timer()
+        self.PathPlanner = PathPlanner("/home/lvuser/py/paths/Forwardybackwardy.json", self.timer)
+        self.limelight=Limelight()
+
+    def autonomousInit(self):
+        self.timer.reset()
+        self.timer.start()
+
+    def autonomousPeriodic(self):
+        self.PathPlanner.run(self.drive)
+
+    def teleopPeriodic(self):
+        
+        if self.follow_limelight_btnid():
+            offset = self.limelight.getoffset()
+            self.drive.move(-offset[0], offset[1], -offset[2])
+        x, y, z = (
+            self.stick.getX(),
+            self.stick.getY(),
+            self.stick.getZ(),
+        )
+        self.drive.move(x, y, z)
+
+
 if __name__ == "__main__":
     wpilib.run(Arpeggio)
