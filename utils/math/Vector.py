621a78fcdab9203c3071896ff7b75e17d1d0ec46
Cinnamonsroll <cinnamonsrowolls@gmail.com>
2024-01-22 08:37:15 -0500

    More vector methods

--- utils/math/Vector.py
+++ utils/math/Vector.py
@@ -7,7 +7,35 @@ class Vector:
         self.c = c
 
     def pythagorean(self, point: 'Vector') -> float:
-        return ((self.a - point.a) ** 2 + (self.b - point.b) ** 2) ** 0.5
+        return ((self.a - point.a) ** 2 + (self.b - point.b) ** 2 + (self.c - point.c) ** 2) ** 0.5
     
     def map(self, func: Callable[[float, int], Union[float, int]]):
-        return (func(self.a), func(self.b), func(self.c))
+        return Vector(func(self.a), func(self.b), func(self.c))
+
+    def __add__(self, other: 'Vector') -> 'Vector':
+        return Vector(self.a + other.a, self.b + other.b, self.c + other.c)
+
+    def __sub__(self, other: 'Vector') -> 'Vector':
+        return Vector(self.a - other.a, self.b - other.b, self.c - other.c)
+
+    def __mul__(self, scalar: float) -> 'Vector':
+        return Vector(self.a * scalar, self.b * scalar, self.c * scalar)
+
+    def dot_product(self, other: 'Vector') -> float:
+        return self.a * other.a + self.b * other.b + self.c * other.c
+
+    def cross_product(self, other: 'Vector') -> 'Vector':
+        return Vector(
+            self.b * other.c - self.c * other.b,
+            self.c * other.a - self.a * other.c,
+            self.a * other.b - self.b * other.a
+        )
+
+    def magnitude(self) -> float:
+        return (self.a ** 2 + self.b ** 2 + self.c ** 2) ** 0.5
+
+    def normalize(self) -> 'Vector':
+        mag = self.magnitude()
+        if mag == 0:
+            return Vector(0, 0, 0)
+        return Vector(self.a / mag, self.b / mag, self.c / mag)
