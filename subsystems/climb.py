f97eb6709fc5e41f6ff3f068f596e04896d14337
Cinnamonsroll <cinnamonsrowolls@gmail.com>
2024-01-25 20:38:15 -0500

    Fix typing issue and also make it so the motor actually works

--- subsystems/climb.py
+++ subsystems/climb.py
@@ -1,12 +1,14 @@
 from phoenix6 import hardware, controls
 from typing import Union
 import wpilib
+
+from components.motor.talon5533 import Talon5533
 class ClimbSubSystem:
-    def __init__(self, motor): 
+    def __init__(self, motor: Talon5533): 
         self.motor = motor
         self.can_run = True
     def update_state(self, state: bool):
         self.can_run = state
     def move(self, speed: float):
         if self.can_run:
-            self.motor.set_control(speed / 100)
+            self.motor.set(speed / 100)
