c44c49a21c0fa3c92f4a2d2e2d2786be7c4031fc
5533Robotics <5533ehprobotics@gmail.com>
2024-01-28 17:00:00 -0500

    woo

--- robot_container.py
+++ robot_container.py
@@ -8,9 +8,10 @@ class RobotContainer:
         self.subsystems = subsystems
         self.stick = stick
         self.teleop_lock = Lockdown()
-        ActionMap.register_action("activate_limelight", self.teleop_lock.lockify(lambda: self.stick.getRawButton(1)))
-        ActionMap.register_action("activate_climb", self.teleop_lock.lockify(lambda: self.stick.getRawButton(2)))
-        ActionMap.register_action("activate_gyro", self.teleop_lock.lockify(lambda: self.stick.getRawButton(3)))
+        self.action_map = ActionMap()
+        self.action_map.register_action("activate_limelight", self.teleop_lock.lockify(lambda: self.stick.getRawButton(1)))
+        self.action_map.register_action("activate_climb", self.teleop_lock.lockify(lambda: self.stick.getRawButton(2)))
+        self.action_map.register_action("activate_gyro", self.teleop_lock.lockify(lambda: self.stick.getRawButton(3)))
         
     def get_motion(self):
         return self.stick.getX(), self.stick.getY(), self.stick.getZ()
@@ -19,7 +20,7 @@ class RobotContainer:
          x, y, z = self.get_motion()
          self.subsystems.setup(
             self.subsystems.limelight.correct_error,
-            ActionMap.get_action_pressed("activate_limelight"),
+            self.action_map.get_action_pressed("activate_limelight"),
             [self.subsystems.drive, self.subsystems.limelight],
             
             self.subsystems.drive
@@ -27,7 +28,7 @@ class RobotContainer:
          
          self.subsystems.setup(
             self.subsystems.climb.move, 
-            ActionMap.get_action_pressed("activate_climb"),
+            self.action_map.get_action_pressed("activate_climb"),
             [self.subsystems.climb],
             
             -y
@@ -35,11 +36,11 @@ class RobotContainer:
          
          self.subsystems.setup(
             self.subsystems.gyro.update_angle,
-            ActionMap.get_action_pressed("activate_gyro"),
+            self.action_map.get_action_pressed("activate_gyro"),
             [self.subsystems.drive, self.subsystems.gyro],
         )
          print(x, y, z)
-         if not ActionMap.get_action_pressed("activate_climb"):
+         if not self.action_map.get_action_pressed("activate_climb"):
            self.subsystems.drive.move(Vector(x, y, z))
            
          self.subsystems.reset()
