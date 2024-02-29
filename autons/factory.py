682234416c0465d660aefa4f1fe21650e36ed3ed
5533Robotics <132020260+5533Robotics@users.noreply.github.com>
2024-02-29 17:15:14 -0500

    Attempt to move left

--- autons/factory.py
+++ autons/factory.py
@@ -1,8 +1,9 @@
 from autons.autons.FourPiece import FourPiece
+from autons.autons.LeftPiece import LeftPiece
 from autons.autons.TwoPiece import TwoPiece
 from subsystems.index import SubSystems
 
-AutonList = ["TwoPiece", "FourPiece"]
+AutonList = ["TwoPiece", "FourPiece", "LeftPiece"]
 
 def create_auton(subsystems: SubSystems, type: str, timer):
         match type:
@@ -10,3 +11,5 @@ def create_auton(subsystems: SubSystems, type: str, timer):
                 return TwoPiece(subsystems, timer)
             case "FourPiece":
                   return FourPiece(subsystems, timer)
+            case "LeftPiece":
+                  return LeftPiece(subsystems, timer)
