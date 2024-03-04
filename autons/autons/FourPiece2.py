bc26d3911d63e21c87d0ba3b0e9814632ed2a958
5533Robotics <132020260+5533Robotics@users.noreply.github.com>
2024-03-03 19:44:19 -0500

    This is the branch we are going to use only at kettering at tuesday, our auton is the original fast 2 piece and then uses mecanum to attempt to grab the third, it almost gets it

--- autons/autons/FourPiece2.py
+++ autons/autons/FourPiece2.py
@@ -8,17 +8,11 @@ class FourPiece2(Auton):
         super().__init__(subsystems, timer)
         self.subsystems = subsystems
 
-    def run(self):
-        
-        # Shoot first note
-        self.shoot(speed = Auton.Speeds.shooter, direction = 1, duration = 0.5)
-        self.intake(speed = Auton.Speeds.intake, direction = -1, duration = 0.45)
-        self.stop()
 
-        # Grab Second Note
+    def get_note(self):
         self.flip(direction = -1, duration = 0.3)
         self.flip(direction = 1, duration = 0.1, speed = 0)
-        self.intake(direction = 1, duration = 0.7, speed = Auton.Speeds.intake)
+        self.intake(direction = 1, duration = 0.9, speed = Auton.Speeds.intake)
         self.drive(velocity = Vector(0, -0.7, self.subsystems.gyro.calculate(0)), duration = 0.5)
         self.drive(velocity = Vector(0, 0, self.subsystems.gyro.calculate(0)), duration = 0.3, brake = True)
         self.intake(speed = 0, direction = 0, duration = 0.2)
@@ -27,32 +21,31 @@ class FourPiece2(Auton):
         self.drive(velocity = Vector(0, 0.9, self.subsystems.gyro.calculate(0)), duration = 0.4)
         self.drive(velocity = Vector(0, 0, 0), duration = 0.3, brake = True)
 
+
+    def run(self):
+        
+        # Shoot first note
+        self.shoot(speed = Auton.Speeds.shooter, direction = 1, duration = 0.5)
+        self.intake(speed = Auton.Speeds.intake, direction = -1, duration = 0.45)
+        self.stop()
+
+        # Grab Second Note
+        self.get_note()
+
         # Shoot second note
         self.shoot(speed = Auton.Speeds.shooter, direction = 1, duration = 0.5)
         self.intake(speed = Auton.Speeds.intake, direction = -1, duration = 0.45)
         self.stop()
 
-        # Rotate to third piece
-        self.drive(velocity = Vector(0, 0, self.subsystems.gyro.calculate(35)), duration = 0.3)
-
-        # Drive back to speaker
-        self.flip(direction = -1, duration = 0.3)
-        self.flip(direction = 1, duration = 0.1, speed = 0)
-        self.intake(direction = 1, duration = 0.7, speed = Auton.Speeds.intake)
-        self.drive(velocity = Vector(0, -0.7, self.subsystems.gyro.calculate(35)), duration = 0.6)
+        # Grab third
+        self.drive(velocity = Vector(-0.7, 0, self.subsystems.gyro.calculate(0)), duration = 0.5)
+        self.drive(velocity = Vector(0, 0, self.subsystems.gyro.calculate(0)), duration = 0.3, brake = True)
+        self.get_note()
+        self.drive(velocity = Vector(0.9, 0, self.subsystems.gyro.calculate(0)), duration = 0.4)
         self.drive(velocity = Vector(0, 0, 0), duration = 0.3, brake = True)
-        
-        self.intake(speed = 0, direction = 0, duration = 0.2)
-        self.flip(direction = 1, duration = 0.5)
-        self.flip(direction = -1, duration = 0.1, speed = 0)
-        self.drive(velocity = Vector(0, 0.9, self.subsystems.gyro.calculate(35)), duration = 0.4)
-        self.drive(velocity = Vector(0, 0, 0), duration = 0.3, brake = True)
-        
+      
 
-        # Shoot Third note
-        self.shoot(speed = Auton.Speeds.shooter, direction = 1, duration = 0.5)
-        self.intake(speed = Auton.Speeds.intake, direction = -1, duration = 0.45)
-        self.stop()
+        
         # self.rotate(angle = 180)
         
         self.tasks.reset()
