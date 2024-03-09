948b7cbd2684616dccd564c259cc5e6f4343aada
5533Robotics <5533ehprobotics@gmail.com>
2024-03-09 10:44:21 -0500

    Turn right two piece unstable

--- autons/autons/TwoPieceRight.py
+++ autons/autons/TwoPieceRight.py
@@ -1,27 +1,28 @@
 from autons.auton2 import Auton
+from autons.autons.OnePieceNoTaxi import OnePieceNoTaxi
+from autons.autons.TwoPiece import TwoPiece
 from subsystems.index import SubSystems
 from utils.math.Vector import Vector
 from utils.math.algebra import linear_remap
-
-
-class OneTaxi(Auton):
+from wpilib import DriverStation
+class TwoPieceRight(Auton):
     def __init__(self, subsystems: SubSystems, timer):
         super().__init__(subsystems, timer)
         self.subsystems = subsystems
-        
+        self.one_piece = OnePieceNoTaxi(subsystems, timer, self.tasks)
 
 
-    def get_note(self):
+    def get_note(self, last_duration = 0.425, first_duration = 0.3):
         self.flip(direction = -1, duration = 0.3)
         self.flip(direction = 1, duration = 0.1, speed = 0)
         self.intake(direction = 1, duration = 0.9, speed = Auton.Speeds.intake)
-        self.drive(velocity = Vector(0, -0.9, self.subsystems.gyro.calculate(0)), duration = 0.35)
+        self.drive(velocity = Vector(0, -0.9, self.subsystems.gyro.calculate(0)), duration = first_duration)
         self.drive(velocity = Vector(0, 0, self.subsystems.gyro.calculate(0)), duration = 0.5, brake = True)
-        self.intake(speed = 0, direction = 0, duration = 0.2)
+
         self.flip(direction = 1, duration = 0.5)
         self.flip(direction = -1, duration = 0.1, speed = 0)
-        self.drive(velocity = Vector(0, 0.9, self.subsystems.gyro.calculate(0)), duration = 0.425)
-        self.drive(velocity = Vector(0, 0, self.subsystems.gyro.calculate(0)), duration = 0.3, brake = True)
+        self.intake(speed = 0, direction = 0, duration = 0.2)
+        
     
     def shoot_note(self):
         self.shoot(speed = Auton.Speeds.shooter, direction = 1, duration = 0.7)
@@ -34,18 +35,28 @@ class OneTaxi(Auton):
     
     def move_right(self, duration: float, speed: float):
         self.move_left(duration, -speed)
-    def initate(self):
-        self.drive(velocity = Vector(0, -0.9, self.subsystems.gyro.calculate(0)), duration = 0.3)
-        self.drive(velocity = Vector(0, 0, 0), duration = 0.3, brake = True)
-        # Shoot first note
-        self.shoot(speed = Auton.Speeds.shooter, direction = 1, duration = 0.9)
-        self.intake(speed = Auton.Speeds.intake, direction = -1, duration = 0.45)
-        self.stop()
+
     def run(self):
-        self.initate()
+        self.one_piece.initate()
         
-        # Taxi
-        self.drive(velocity = Vector(0, -0.9, self.subsystems.gyro.calculate(0)), duration = 0.9)
+        
+        self.drive(velocity = Vector(0, 0, -0.15), duration = 0.3)
         self.drive(velocity = Vector(0, 0, 0), duration = 0.3, brake = True)
+        
+        @self.tasks.timed_task(0.1, self.subsystems)
+        def reset_gyro(subsystems: SubSystems):
+            subsystems.gyro.reset()
+            
+        self.get_note()
+        
+        self.drive(velocity = Vector(0, 0, 0.15), duration = 0.3)
+        self.drive(velocity = Vector(0, 0, 0), duration = 0.3, brake = True)
+        
+        self.one_piece.initate()
+        
+       # Taxi
+        self.drive(velocity = Vector(0, -0.9, self.subsystems.gyro.calculate(0)), duration = 0.7)
+        self.drive(velocity = Vector(0, 0, self.subsystems.gyro.calculate(0)), duration = 0.3, brake = True)
         
         self.tasks.reset()
+
