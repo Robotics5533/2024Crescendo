01298c4ba6ad62964e606f8cb63e18f37c86c4e7
5533Robotics <132020260+5533Robotics@users.noreply.github.com>
2024-03-12 17:14:57 -0400

    We had a competition?

--- autons/autons/ThreePieceLeftUnstable.py
+++ autons/autons/ThreePieceLeftUnstable.py
@@ -4,7 +4,7 @@ from subsystems.index import SubSystems
 from utils.math.Vector import Vector
 from utils.math.algebra import linear_remap
 from wpilib import DriverStation
-class ThreePieceRightUnstable(Auton):
+class ThreePieceLeftUnstable(Auton):
     def __init__(self, subsystems: SubSystems, timer):
         super().__init__(subsystems, timer)
         self.subsystems = subsystems
