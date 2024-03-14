c1d73ca923c8016ee2c4bfdbe6278ef48e7df2ef
5533Robotics <132020260+5533Robotics@users.noreply.github.com>
2024-03-13 21:30:21 -0400

    update gyro calculate to use pid

--- subsystems/gyro.py
+++ subsystems/gyro.py
@@ -1,8 +1,12 @@
+from wpimath.controller import PIDController
+
 
 class GyroSubSystem:
     def __init__(self, gyroscope): 
         self.gyro = gyroscope
         self.can_run = True
+        # self.pid = PIDController(3/360, .3/360, 0.5/360) # PID values for 180
+        self.pid = PIDController(7/360, 4/360, .6/360)
         
     def update_state(self, state: bool):
         self.can_run = state
@@ -12,15 +16,16 @@ class GyroSubSystem:
 
 
     def calculate(self, target_angle: float):
-        angle = self.gyro.getAngle()
-        error = target_angle - angle
-        if abs(error) >= 180:
-            error = 360 - error
-        v = error / 180
-        v = v * 7
-        if abs(v) > 1:
-           v = v / abs(v)
-        return v
+        return self.pid.calculate(self.gyro.getAngle(), target_angle)
+        # angle = self.gyro.getAngle()
+        # error = target_angle - angle
+        # if abs(error) >= 180:
+        #     error = 360 - error
+        # v = error / 180
+        # v = v * 7
+        # if abs(v) > 1:
+        #    v = v / abs(v)
+        # return v
         
 
     def run(self, speed: float):
